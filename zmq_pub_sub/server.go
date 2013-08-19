25c3318c44045df0155caa635ed931fce18f2f6f
AbhishekKr <abhikumar163@gmail.com>
2013-08-19 23:56:53 +0000

    very basic Server to PubSub clients for PullPub facility

---
+++ zmq_pub_sub/server.go
@@ -0,0 +1,33 @@
+package zmq_pub_sub
+
+import (
+	// go packages
+	"time"
+	// 3party packages
+	zmq "github.com/alecthomas/gozmq"
+)
+
+func Server() {
+  println("Starting Chaac's Hand. Giving from 8080 and Receiving from 8081 ;)")
+
+	context, _ := zmq.NewContext()
+	receiver, _ := context.NewSocket(zmq.PULL)
+	receiver.Bind("tcp://*:8080")
+	sender, _ := context.NewSocket(zmq.PUB)
+	sender.Bind("tcp://*:8081")
+
+	last := time.Now()
+	messages := 0
+	for {
+		message, _ := receiver.Recv(0)
+		sender.Send(message, 0)
+		messages += 1
+		now := time.Now()
+		if now.Sub(last).Seconds() > 10 {
+			println(messages, "msg/sec")
+			last = now
+			messages = 0
+		}
+	}
+
+}
